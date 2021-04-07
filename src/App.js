3c28e31f84c354bc9da929fe78396b49c07f6569
shun1121 <hikitanikun.ware80000@gmail.com>
2021-04-07 16:32:32 +0900

    tried React.createElement()

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React, {Component} from 'react';
 
 class App extends Component {
   render() {
-    return <div><h1>Hello World!!</h1></div>
+    return <h1>Hello World!!</h1>
   }
 }
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "h1",
+//       null,
+//       "Hello World!!"
+//     )
+//   }
+// }
 
 export default App;
