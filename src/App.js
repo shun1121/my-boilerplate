50a47555df5c4a0856db12efd85df47211c9daa4
shun1121 <hikitanikun.ware80000@gmail.com>
2021-04-07 18:11:32 +0900

    checked the differences of the Component type

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React, {Component} from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="label">label</label>
-        <input type="text" onChange={() => {console.log("tired")}} />
-      </React.Fragment>
-    )
-  }
+//AppがComponentを継承している 
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="label">label</label>
+//         <input type="text" onChange={() => {console.log("tired")}} />
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+// 関数コンポーネント
+const App = () => {
+  return (
+  <div>
+    <Dog />
+    <Cat />
+  </div>)
 }
+
+const Dog = () => {
+  return <div>Bow!</div>
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
+}
+
+// export default App
+
 // class App extends Component {
 //   render() {
 //     return React.createElement(
