8867941b3c6f4033b06e570af7d6960d5d415bd0
shun1121 <hikitanikun.ware80000@gmail.com>
2021-04-08 13:26:24 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+// actionクリエーターの作成
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+    type: INCREMENT
+})
+
+export const decrement = () => {
+    return {
+        type: DECREMENT
+    }
+}
